3c5c88ad1a23f1b80da7548934312f428542aba9
ugocottin <ugo.cottion@gmail.com>
2020-03-09 16:34:32 +0100

    Initial Commit

---
+++ Maze/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Maze
+//
+//  Created by ugo cottin on 09/03/2020.
+//  Copyright © 2020 ugo cottin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
